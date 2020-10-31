c02e27a12cba3f60249ba3c4e5e75aaae501a561
pmma_ <laurentSpam99@gmail.com>
2020-10-31 15:35:27 +0100

    SHitbitch

--- Server/MusicManager.Server/MusicManager.Server/Core/Config/AppSettings.cs
+++ Server/MusicManager.Server/MusicManager.Server/Core/Config/AppSettings.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Threading.Tasks;
 
 namespace MusicManager.Server.Core.Config
@@ -8,5 +9,6 @@ namespace MusicManager.Server.Core.Config
     public class AppSettings
     {
         public string Secret { get; set; }
+        public string ConnectionString { get; set; }
     }
 }
