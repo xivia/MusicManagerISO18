7b069ebdd5316fa6ff142421c2ad6fcda23d35e3
micmine <micmine4@gmail.com>
2020-10-31 14:06:44 +0100

    Add nuxt proxy

--- Frontend/MusicManagment.Frontend/nuxt.config.js
+++ Frontend/MusicManagment.Frontend/nuxt.config.js
@@ -63,7 +63,13 @@ export default {
   ],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
-  axios: {},
+  axios: {
+    proxy: true
+  },
+
+  proxy: {
+    '/api/': 'http://localhost:8080',
+  },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
