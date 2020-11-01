838df82cc1f3505f2c8e5840143dfca3e74cad69
micmine <micmine4@gmail.com>
2020-11-01 09:37:16 +0100

    Remove unused if

--- Frontend/MusicManagment.Frontend/middleware/authenticated.ts
+++ Frontend/MusicManagment.Frontend/middleware/authenticated.ts
@@ -2,9 +2,7 @@ import { Middleware } from '@nuxt/types'
 import { getters } from '~/store/authentication'
 
 const authenticated: Middleware = async ({store, redirect, route}) => {
-        if (route.path.endsWith('/register') || route.path.endsWith('/login')) {
-            //next();
-        } else {
+        if (!route.path.endsWith('/register') && !route.path.endsWith('/login')) {
             if (!store.getters['authentication/isAuthenticated'] as ReturnType<typeof getters.isAuthenticated>) {
                 redirect('/login');
             }
