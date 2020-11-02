f99d5084521b46f6248b1ba35caee5793e84b0d3
pmma_ <laurentSpam99@gmail.com>
2020-11-02 15:11:20 +0100

    aDDED

--- Server/MusicManager.Server/MusicManager.Server/creat_user.sql
+++ Server/MusicManager.Server/MusicManager.Server/creat_user.sql
@@ -1,4 +1,5 @@
 ﻿-- Download and Install SQL Server 2019 https://go.microsoft.com/fwlink/?linkid=866662
+-- THIS SHIT DOESN'T WORK YOU HAVE TO CREATE THE USER MANUALLY
 CREATE LOGIN music_manager WITH PASSWORD = '1337';
 CREATE DATABASE MusicManager;
 EXEC sp_addrolemember N'db_owner', N'music_manager';
