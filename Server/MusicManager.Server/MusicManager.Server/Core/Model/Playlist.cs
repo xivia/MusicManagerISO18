3259474d6e0e0368d20bc89c5fa9dc729800e30a
pmma_ <laurentSpam99@gmail.com>
2021-01-18 14:36:24 +0100

    Yes

--- Server/MusicManager.Server/MusicManager.Server/Core/Model/Playlist.cs
+++ Server/MusicManager.Server/MusicManager.Server/Core/Model/Playlist.cs
@@ -9,7 +9,7 @@ namespace MusicManager.Server.Core.Model
         [Key]
         public long PlaylistId { get; set; }
         [ForeignKey("UserId")]
-        public User user { get; set; }
+        public User User { get; set; }
         public string Name { get; set; }
         public virtual List<Song> Songs { get; set; }
     }
