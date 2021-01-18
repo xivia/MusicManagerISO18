a3c57dc742b335e4ea88dc88d8b1c44ddd5e336f
pmma_ <laurentSpam99@gmail.com>
2021-01-18 13:42:00 +0100

    Added validate mp3 header signature

---
+++ Server/MusicManager.Server/MusicManager.Server/Core/Validators/FileTypeValidators/FileTypeVerifier.cs
@@ -0,0 +1,47 @@
+﻿using MusicManager.Server.Core.Validators.FileTypeValidators.FileTypes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicManager.Server.Core.Validators.FileTypeValidators
+{
+    public static class FileTypeVerifier
+    {
+        private static FileTypeVerifyResult Unknown = new FileTypeVerifyResult
+        {
+            Name = "Unknown",
+            Description = "Unknown File Type",
+            IsVerified = false
+        };
+
+        static FileTypeVerifier()
+        {
+            Types = new List<FileType>
+            {
+                new Mp3()
+            }
+            .OrderByDescending(x => x.SignatureLength)
+            .ToList();
+        }
+
+        private static IEnumerable<FileType> Types { get; set; }
+
+        public static FileTypeVerifyResult What(Stream fileReadStream)
+        {
+            FileTypeVerifyResult result = null;
+
+            foreach (var fileType in Types)
+            {
+                result = fileType.Verify(fileReadStream);
+                if (result.IsVerified)
+                    break;
+            }
+
+            return result?.IsVerified == true
+               ? result
+               : Unknown;
+        }
+    }
+}
--- Server/MusicManager.Server/MusicManager.Server/Core/Validators/SongFileValidator.cs
+++ Server/MusicManager.Server/MusicManager.Server/Core/Validators/SongFileValidator.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using Microsoft.AspNetCore.Http;
+using MusicManager.Server.Core.Validators.FileTypeValidators;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +15,13 @@ namespace MusicManager.Server.Core.Validators
         public SongFileValidator()
         {
             // TODO: Validate file header signature
+            RuleFor(x => x).Custom((file, context) => 
+            {
+                var result = FileTypeVerifier.What(file.OpenReadStream());
+                
+                if(!result.IsVerified)
+                    context.AddFailure("The file has an invalid file type");
+            });
 
             // length / 1048576 gives size in mb
             RuleFor(x => x.Length / 1048576).NotNull().LessThanOrEqualTo(MAX_FILE_SIZE_MB)
