c8c260b705da18e7b68ab0d0202a32639b64a2db
pmma_ <laurentSpam99@gmail.com>
2021-01-18 14:20:18 +0100

    Removed unused imports

--- Server/MusicManager.Server/MusicManager.Server/Core/Validators/FileTypeValidators/FileTypes/Mp3.cs
+++ Server/MusicManager.Server/MusicManager.Server/Core/Validators/FileTypeValidators/FileTypes/Mp3.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace MusicManager.Server.Core.Validators.FileTypeValidators.FileTypes
+﻿namespace MusicManager.Server.Core.Validators.FileTypeValidators.FileTypes
 {
     public sealed class Mp3 : FileType
     {
