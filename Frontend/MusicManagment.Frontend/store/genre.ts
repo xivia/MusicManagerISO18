7e56542866ac5eaa63c3462ce57cf77da6474739
micmine <micmine4@gmail.com>
2021-03-01 07:48:30 +0100

    Fix genre store

--- Frontend/MusicManagment.Frontend/store/genre.ts
+++ Frontend/MusicManagment.Frontend/store/genre.ts
@@ -1,34 +1,33 @@
 import { GetterTree, ActionTree, MutationTree } from 'vuex'
 
 export const state = () => ({
-    genres: []
+  genres: [],
 })
 
-export type RootState = ReturnType<typeof state>;
+export type RootState = ReturnType<typeof state>
 
 export const getters: GetterTree<RootState, RootState> = {
-    genres: state => state.genres,
+  genres: (state) => state.genres,
 }
 
 export const mutations: MutationTree<RootState> = {
-    CHANGE_GENRES: (state, newGenres) => (state.genres = newGenres),
+  CHANGE_GENRES: (state, newGenres) => (state.genres = newGenres),
 }
 
 export const actions: ActionTree<RootState, RootState> = {
-    async getGenres({commit}) {
-        const response = await this.$axios.$get('/api/genre/')
+  async getGenres({ commit }) {
+    const response = await this.$axios.$get('/api/genre/')
 
-        if (!response.infos.hasErrors) {
-            commit('CHANGE_GENRES', response.data);
-        } else {
-            console.error(response);
-        }
-
-    },
-    async createGenre({commit, actions}, data) {
-        const response = await this.$axios.$post('/api/genre', {
-            genreName: data.newGenre
-        });
-        console.log(response);
+    if (!response.infos.hasErrors) {
+      commit('CHANGE_GENRES', response.data)
+    } else {
+      console.error(response)
     }
+  },
+  async createGenre({ commit }, data) {
+    const response = await this.$axios.$post('/api/genre', {
+      genreName: data.newGenre,
+    })
+    console.log(response)
+  },
 }
